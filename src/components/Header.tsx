2a49bfd4999cdfbac897e2d253b7ad9e2780f1f9
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-09-02 03:51:11 -0400

    Create Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react'
+
+const Header: FC = () => {
+  return (
+    <div>
+      <h1> Lorem, ipsum dolor sit amet</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit laborum quisquam sequi. Voluptatum
+        dignissimos sequi sapiente perspiciatis quod, eum repudiandae laudantium libero provident quaerat repellat eius
+        officia. Totam, nemo provident. Totam, eum excepturi tenetur alias optio labore praesentium minus iure modi ex,
+        molestiae recusandae placeat nesciunt aliquid quo dolorem sequi unde dolore eius perspiciatis architecto harum
+        sint repudiandae impedit. Rerum. Similique, vero, pariatur expedita ab in porro non at amet quod debitis
+        provident voluptas ex sapiente doloribus deserunt sunt. Ea quos omnis maxime voluptas vero cum deleniti
+        reiciendis commodi odit?
+      </p>
+      <p>
+        Totam, eum excepturi tenetur alias optio labore praesentium minus iure modi ex, molestiae recusandae placeat
+        nesciunt aliquid quo dolorem sequi unde dolore eius perspiciatis architecto harum sint repudiandae impedit.
+        Rerum.
+      </p>
+    </div>
+  )
+}
+
+export default Header
